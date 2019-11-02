39040cb9da88bb86d537051441dff34dd40b8891
Rizhi_KotE <d1mqn1ch@mail.ru>
2019-11-02 18:02:42 +0300

    Extract util functions

--- design_lemma/test/design_lemmaTest.cpp
+++ design_lemma/test/design_lemmaTest.cpp
@@ -2,24 +2,57 @@
 // in one cpp file
 
 #include "catch2/catch.hpp"
+#include <RelationStructureToVectors.h>
 #include <RelationalStructureTextRepresentation.h>
+#include <design_lemma.h>
 
 using namespace std;
 
-TEST_CASE("Binary RelationalStructure") {
+TEST_CASE("Design lemma for 4 node graph") {
   // clang-format off
   RelationalStructure given = toRelationalStructure(
       {{0, 1, 1, 0},
        {1, 0, 1, 0},
        {1, 1, 0, 1},
        {0, 0, 1, 0}});
-  vector<vector<int>> expected = toVectors(
-      {{0, 1, 1, 2},
-       {1, 0, 1, 2},
-       {1, 1, 0, 1},
-       {2, 2, 1, 0}});
+  
+  const Iso &iso = DesignLemma(
+      given, given, .66666666,
+      [](Either<RelationalStructure, ColouredPartition> A,
+         Either<RelationalStructure, ColouredPartition> B) {
+        if (B.isFirst())
+          std::cout << "relational structure: " << A.getFirst()
+                    << "~?~" << B.getFirst() << std::endl;
+        else
+          std::cout << "coloured 2/3-partition: "
+                    << A.getSecond().getMapping() << "~?~"
+                    << B.getSecond().getMapping() << std::endl;
+        return Empty();
+      });
   // clang-format on
-  const vector<std::vector<int>> &actual =
-      RelationalStructureTextRepresentation(given).matrixOfBinaryStructure();
-  REQUIRE(actual == expected);
+  cout << iso;
+}
+
+TEST_CASE("Design lemma for 4 node permuted graph") {
+  // clang-format off
+  RelationalStructure given = toRelationalStructure(
+      {{0, 1, 0, 0},
+       {1, 0, 1, 1},
+       {0, 1, 0, 1},
+       {0, 1, 1, 0}});
+  const Iso &iso = DesignLemma(
+      given, given, .66666666,
+      [](Either<RelationalStructure, ColouredPartition> A,
+         Either<RelationalStructure, ColouredPartition> B) {
+        if (B.isFirst())
+          std::cout << "relational structure: " << A.getFirst()
+                    << "~?~" << B.getFirst() << std::endl;
+        else
+          std::cout << "coloured 2/3-partition: "
+                    << A.getSecond().getMapping() << "~?~"
+                    << B.getSecond().getMapping() << std::endl;
+        return Empty();
+      });
+  // clang-format on
+  cout << iso;
 }
